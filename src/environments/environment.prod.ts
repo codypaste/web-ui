f476ec293d96180e23cca421b4e1aa1e2dae003b
Daniel Kawka <dkawka@future-processing.com>
2018-11-09 14:46:38 +0100

    Fix prod environemnt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  maxEditors: 5
+  maxEditors: 5,
+  snippetsEndpoint: 'http://localhost:3000/snippets',
+  groupsEndpoint: 'http://localhost:3000/groups',
 };

aaad0a6d7c213c649352ab652f0efc51ffe254e7
Daniel Kawka <ileev@hotmail.com>
2018-11-15 01:24:25 +0100

    Remove console.log

--- src/app/new-project-page/editor/editor.directive.ts
+++ src/app/new-project-page/editor/editor.directive.ts
@@ -27,7 +27,6 @@ export class EditorDirective implements OnDestroy {
     this.activeEditor$ = store.pipe(select(getActiveEditor));
 
     this.editorContent = this.activeEditor$.subscribe(editor => {
-      console.log(editor.content);
       this.editor.setValue(editor.content || '');
       const currentMode = this.editor.getOption('mode');
       let newMode = editor.syntax;
